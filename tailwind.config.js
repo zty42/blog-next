1cb80977399f80c4db9882d2183df6a0dab706f9
zty <tianyuan233.zhang@gmail.com>
2023-01-12 13:16:09 +0000

    style: Style library interoperability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./_posts/**/*.mdx",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  important: '#__next',
   theme: {
     extend: {},
   },
