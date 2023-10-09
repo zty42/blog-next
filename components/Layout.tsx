e706114115b6517737338ff3ba9c15b248dcef90
zty <zty.dev@outlook.com>
2023-10-09 15:40:30 +0800

    style: add a logo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
-
+import Image from "next/image";
+import Logo from "../assets/logo.svg";
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -11,23 +12,20 @@ const Layout = ({ children }: LayoutProps) => {
       <header>
         <nav className="h-[60px] leading-[60px] flex justify-between items-center">
           <Link href="/" className="no-underline">
-            <div className="logo text-2xl font-bold">INDEX</div>
+            <div className="logo text-2xl font-bold">
+              <Image alt="logo" src={Logo} width={40} height={40} className="my-0 inline-block" />
+              <span className="ml-2"></span>
+            </div>
           </Link>
           <div className="flex gap-3">
             <Link href="/archive">
-              <div className="hover:underline">
-                归档
-              </div>
+              <div className="hover:underline">归档</div>
             </Link>
             <Link href="/tags">
-              <div className="hover:underline">
-                标签
-              </div>
+              <div className="hover:underline">标签</div>
             </Link>
             <Link href="/about">
-              <div className="hover:underline">
-                关于
-              </div>
+              <div className="hover:underline">关于</div>
             </Link>
           </div>
         </nav>
