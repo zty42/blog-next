96fa3be6d89e8a4ce5e2882524d71ada682d82d9
tianyuan233 <zty.dev@outlook.com>
2023-10-11 21:46:14 +0800

    chore: remove layout head

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,52 +14,48 @@ const DarkModeButton = dynamic(() => import("./DarkModeButton"), {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <>
-    <Head>
-      <title>zty's blog</title>
-    </Head>
-    <div
-      className="flex flex-col 
-    min-h-screen px-4 mx-auto
-    antialiased 
-    prose prose-sm
-    dark:prose-invert
-    "
-    >
-      <header>
-        <nav className="h-[60px] leading-[60px] flex justify-between items-center">
-          <Link href="/" className="no-underline">
-            <div className="logo text-2xl font-bold">
-              <Image
-                alt="logo"
-                src="/logo.svg"
-                width={40}
-                height={40}
-                className="my-0 inline-block hover:rotate-12 transition-all duration-300"
-              />
-              <span className="ml-2"></span>
+      <div
+        className="flex flex-col 
+          min-h-screen px-4 mx-auto
+          antialiased 
+          prose prose-sm
+          dark:prose-invert"
+      >
+        <header>
+          <nav className="h-[60px] leading-[60px] flex justify-between items-center">
+            <Link href="/" className="no-underline">
+              <div className="logo text-2xl font-bold">
+                <Image
+                  alt="logo"
+                  src="/logo.svg"
+                  width={40}
+                  height={40}
+                  className="my-0 inline-block hover:rotate-12 transition-all duration-300"
+                />
+                <span className="ml-2"></span>
+              </div>
+            </Link>
+            <div className="flex gap-3">
+              <Link href="/archive" className="no-underline">
+                <div className="hover-link">归档</div>
+              </Link>
+              <Link href="/tags" className="no-underline">
+                <div className="hover-link">标签</div>
+              </Link>
+              <Link href="/about" className="no-underline">
+                <div className="hover-link">关于</div>
+              </Link>
+              <div className="w-6 flex items-center">
+                <DarkModeButton />
+              </div>
             </div>
-          </Link>
-          <div className="flex gap-3">
-            <Link href="/archive" className="no-underline">
-              <div className="hover-link">归档</div>
-            </Link>
-            <Link href="/tags" className="no-underline">
-              <div className="hover-link">标签</div>
-            </Link>
-            <Link href="/about" className="no-underline">
-              <div className="hover-link">关于</div>
-            </Link>
-            <div className="w-6 flex items-center">
-              <DarkModeButton />
-            </div>
-          </div>
-        </nav>
-      </header>
-      <main className="flex-grow flex flex-col w-full">{children}</main>
-      <footer className="text-center flex-shrink-0 py-4">
-        © {new Date().getFullYear()}, authored by zty.
-      </footer>
-    </div>
+          </nav>
+        </header>
+        <main className="flex-grow flex flex-col w-full">{children}</main>
+        <footer className="text-center flex-shrink-0 py-4">
+          © {new Date().getFullYear()}, authored by zty.
+        </footer>
+      </div>
     </>
   );
 };
