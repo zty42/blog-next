14937e547d50b9f70e462940184754c4a37ffc8f
zty <zty.dev@outlook.com>
2023-10-13 14:17:40 +0800

    feat: add gfm plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
             maxWidth: "80ch",
             '--tw-prose-invert-links':theme.colors.gray[300],
             '--tw-prose-invert-headings':theme.colors.gray[300],
+            '--tw-prose-invert-bullets':theme.colors.gray[300],
           },
         },
       }),
