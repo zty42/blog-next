ca3774a993f8b916f579553f0d8c4c98dc73593b
tianyuan233 <zty.dev@outlook.com>
2023-10-13 21:22:16 +0800

    xxx

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -2,13 +2,14 @@ import { bundleMDX } from "mdx-bundler";
 import { readFile } from "node:fs/promises";
 import { Frontmatter } from "../@types";
 import rehypePrettyCode from "rehype-pretty-code";
+import remarkGfm from "remark-gfm";
 
 export async function transMdx(path: string) {
   const fileContents = await readFile(path, "utf8");
   const { code, frontmatter } = await bundleMDX({
     source: fileContents,
-    mdxOptions(options) {
-      options.remarkPlugins = [...(options.remarkPlugins ?? [])];
+    mdxOptions(options, frontmatter) {
+      options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkGfm];
       options.rehypePlugins = [
         ...(options.rehypePlugins ?? []),
         [
