9e938d6ae978f67485aba4b7ad5b216a67e7c8c3
zty <zty.dev@outlook.com>
2023-10-23 17:21:43 +0800

    add gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-WRS7XX17K4" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-WRS7XX17K4');
+        `}
+        </Script>
       </Head>
       <body>
         <script src="/noflash.js" async />
