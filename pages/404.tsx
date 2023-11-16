04d7b2485231a518ce2062bc5044cc9ec9763f41
zty <zty.dev@outlook.com>
2023-11-16 10:18:29 +0800

    Add title to 404 page and wrap content in Head tag

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,18 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <div className="flex flex-col justify-center items-center flex-1">
-      <p>404</p>
-      <Link href="/" className="no-underline">
-        <div className="hover-link">返回首页</div>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>404</title>
+      </Head>
+      <div className="flex flex-col justify-center items-center flex-1">
+        <p>404</p>
+        <Link href="/" className="no-underline">
+          <div className="hover-link">返回首页</div>
+        </Link>
+      </div>
+    </>
   );
 }
