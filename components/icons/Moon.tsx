d6470aa84086958e0653b810c49822a5a17e1689
zty <zty.dev@outlook.com>
2023-11-16 14:52:54 +0800

    Fix stroke attributes in Moon and Sun icons.

--- components/icons/Moon.tsx
+++ components/icons/Moon.tsx
@@ -7,9 +7,9 @@ export default function Moon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
     </svg>
--- components/icons/Sun.tsx
+++ components/icons/Sun.tsx
@@ -7,10 +7,9 @@ export default function Sun() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <circle cx="12" cy="12" r="4" />
       <path d="M12 2v2" />
