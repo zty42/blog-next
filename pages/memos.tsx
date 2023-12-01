26109c083fe854f331a91c94842c3392e86833a7
zty <zty.dev@outlook.com>
2023-12-01 10:58:13 +0800

    Update runtime to experimental-edge

--- pages/memos.tsx
+++ pages/memos.tsx
@@ -21,7 +21,7 @@ interface Memo {
   creatorName: string;
   creatorUsername: string;
 }
-export const runtime = 'edge';
+export const runtime = 'experimental-edge';
 
 export async function getServerSideProps() {
   const res = await fetch(
