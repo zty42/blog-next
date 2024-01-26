c545d0e918819de3413d5943c9ba4915a3b0ac24
zty <zty.dev@outlook.com>
2024-01-26 10:12:27 +0800

    Refactor logo component to use lucide-react icons

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,29 +1,13 @@
 import Image from "next/image";
-
-import { Button } from "./ui/button";
 import Link from "next/link";
-
+import { Code2 } from "lucide-react";
 export const Logo = () => {
   return (
     <div className="flex items-center gap-x-2 h-full">
       <Link href="/" className="no-underline">
-        <Image
-          src="/logo.svg"
-          height="20"
-          width="20"
-          alt="Logo"
-          className="dark:hidden inline-block"
-        />
-        <Image
-          src="/logo-dark.svg"
-          height="20"
-          width="20"
-          alt="Logo"
-          className="hidden dark:inline-block"
-        />
-        <span className="ml-2">
-        zty&apos;s blog
-        </span>
+        <Code2 className="dark:hidden inline-block" />
+        <Code2 className="hidden text-[#f8fafc] dark:inline-block" />
+        <span className="ml-2">zty&apos;s blog</span>
       </Link>
     </div>
   );
