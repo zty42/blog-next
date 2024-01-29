1cbb4ca88c570f7350e32b082e1e2f1cf26c5241
zty <zty.dev@outlook.com>
2024-01-29 14:46:29 +0800

    Refactor footer and api modules

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
-
+import { Github } from "lucide-react";
 export const Footer = () => {
   return (
     <footer className="text-center flex-shrink-0 py-4">
-      ©{new Date().getFullYear()}, authored by <Link href="https://github.com/zty42">zty</Link>.
+      ©{new Date().getFullYear()}, authored by{" "}
+      <Link href="https://github.com/zty42">
+        <Github className="inline w-4 h-4" />
+        zty
+      </Link>
     </footer>
   );
 };
--- lib/api.ts
+++ lib/api.ts
@@ -1,40 +1,51 @@
-import path from 'path'
-import fs from 'fs/promises'
-import { cache } from 'react'
-import { glob } from 'glob'
-import { transMdx } from './mdx'
+import path from "path";
+import fs from "fs/promises";
+import { cache } from "react";
+import { glob } from "glob";
+import { transMdx } from "./mdx";
 
-import type { Post } from '@/types/post'
+import type { Post } from "@/types/post";
 
 export const getPosts = cache(async (): Promise<string[]> => {
-  const posts = await glob("_posts/**")
-  return posts
-})
+  const posts = await glob("_posts/**");
+  return posts;
+});
 
 export const getAllPosts = cache(async () => {
-  const posts = await glob(`_posts/**/*.{md,mdx}`)
-  return await Promise.all(
+  const posts = await glob(`_posts/**/*.{md,mdx}`);
+  const res = await Promise.all(
     posts.map(async (post): Promise<Post> => {
       const slug = path.basename(post).replace(/\.mdx?$/, "");
       const fileContent = await fs.readFile(post, "utf8");
-      const { code, frontmatter } = await transMdx(fileContent)
+      const { code, frontmatter } = await transMdx(fileContent);
       return {
         slug,
         code,
-        frontmatter
-      }
+        frontmatter,
+      };
     })
   );
-})
+  return res.sort((a, b) => {
+    return (
+      new Date(b.frontmatter.date).getTime() -
+      new Date(a.frontmatter.date).getTime()
+    );
+  });
+});
 
 export const getIndexPosts = cache(async (): Promise<Post[]> => {
-  const posts = await getAllPosts()
-  return posts.sort((a, b) => {
-    return new Date(b.frontmatter.date).getTime() - new Date(a.frontmatter.date).getTime()
-  }).slice(0, 5)
-})
+  const posts = await getAllPosts();
+  return posts
+    .sort((a, b) => {
+      return (
+        new Date(b.frontmatter.date).getTime() -
+        new Date(a.frontmatter.date).getTime()
+      );
+    })
+    .slice(0, 5);
+});
 
 export const getPostBySlug = cache(async (slug: string): Promise<Post> => {
-  const posts = await getAllPosts()
-  return posts.find((post) => post.slug === slug) as Post
-})
+  const posts = await getAllPosts();
+  return posts.find((post) => post.slug === slug) as Post;
+});
