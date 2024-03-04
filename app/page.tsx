e58f8a7618389c4db82b1a8be5ac9dc860801f5d
zty <zty.dev@outlook.com>
2024-03-04 11:20:21 +0800

    Add header component to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/header";
 import { getAllPosts } from "@/lib/api";
 import Link from "next/link";
 
@@ -5,25 +6,25 @@ export default async function HomePage() {
   const posts = await getAllPosts();
   return (
     <main className="flex flex-col">
-      <header className="my-8">
-        <article>
-          <p>个人博客，文字、代码、照片，记录工作和生活</p>
-        </article>
-      </header>
-
+      <Header />
       <div>
         {posts.map((post) => {
           return (
-            <Link
+            <div
               key={post.slug}
-              href={`/post/${post.slug}`}
-              className="py-3 flex flex-col-reverse md:flex-row"
+              className="py-3 flex flex-col-reverse md:flex-row hover:bg-[#fcf5f5]"
             >
               <time className="mr-4 opacity-50 md:opacity-100">
                 {post.frontmatter.date}
               </time>
-              <h1>{post.frontmatter.title}</h1>
-            </Link>
+              <Link
+                key={post.slug}
+                href={`/post/${post.slug}`}
+                // className="py-3 flex flex-col-reverse md:flex-row"
+              >
+                <h1>{post.frontmatter.title}</h1>
+              </Link>
+            </div>
           );
         })}
       </div>
---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="my-8">
+      <article>
+        <p>个人博客，文字、代码、照片，记录工作和生活</p>
+      </article>
+    </header>
+  );
+};
