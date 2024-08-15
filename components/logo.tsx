73834c802a4c89debe917404818c822e6a3db434
zty <zty.dev@outlook.com>
2024-08-15 16:03:50 +0800

    update

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
-import { Code2 } from "lucide-react";
+import Image from "next/image";
+import LogoSvg from "@/public/logo.svg";
+
 export const Logo = () => {
   return (
     <div className="flex items-center gap-x-2 h-full font-bold text-3xl z-50">
-      <Link href="/" className="">
+      <Link href="/">
+        <Image src={LogoSvg} alt="logo" width={30} height={30} style={{display: "inline"}} />
         zty&apos;s blog
       </Link>
     </div>
